6e741d559755d7d6d75e0f1caba815ecaae00446
rob <robcannonxyz@gmail.com>
2020-09-17 15:09:48 -0700

    improve packageing

--- setup.py
+++ setup.py
@@ -2,20 +2,27 @@ from setuptools import setup, find_packages
 
 url = ""
 version = "0.1.0"
-readme = open('README.rst').read()
+readme = open('README.md').read()
 
 setup(
-    name="icon-prometheus-exporter",
-    packages=find_packages(),
-    # packages=["icon-prometheus-exporter"],
+    name="icon-network-exporter",
+    packages=["icon_network_exporter"],
     version=version,
     description="exporter agent for icon blockchain",
     long_description=readme,
+    entry_points={
+        'console_scripts': [
+            'icon-network-exporter=icon_network_exporter:main',
+        ],
+    },
+    install_requires=[
+        'requests>=2,<3',
+        'prometheus_client'
+    ],
     include_package_data=True,
     author="Haitham Ghalwash",
     author_email="h.ghalwash@gmail.com",
     url=url,
-    install_requires=[],
     download_url="{}/tarball/{}".format(url, version),
     license="MIT"
 )
